1ba18e0f18061069c40debceb7388f119239a307
Jan5551 <77397588+Jan5551@users.noreply.github.com>
2021-04-10 18:14:51 +0200

    Update vue.js

--- google/vue.js
+++ google/vue.js
@@ -1,14 +1,16 @@
-var myBody = document.getElementsByTagName('body')[0];
+var Body = document.getElementsByTagName('body')[0];
 
 const input = document.querySelector('input');
 input.addEventListener('change', updateValue);
-
+const button = document.querySelector('button');
 function updateValue(e)
 {
     if(e.target.value != '')
     {
-        myBody.classList.toggle('results');
-    }
+        button.addEventListener('click', (e) => {
+  e.preventDefault();
+        Body.classList.toggle('results');
+        }}
     else
     {
     }
